21bd3018410c89f17a10e9eef6901a3fa17197f4
fanzozo <48625823+fanzozo@users.noreply.github.com>
2022-10-17 10:40:35 +0200

    Add files via upload
    assessment 2c program

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
-window.alert('laoding')
-const Name = "Fanie Khotle"
-const school = "Mtn App Academy"
-const username = "fanzozo" 
-let intro = () => {
-    console.log(`Hi I'm ${Name}, a student from ${school} and my github username is ${username}`);
+const fs = require('fs')
+
+fs.open('file.txt','w',(err,file)=> {
+    if (err){
+        console.error(err);
+    return;
 }
-window.onload= intro();
+
+    console.log('file opened');
+    
+})
+
+
+
+
+ const content = "winner of 2021 mtn app of the year campus catergory is Uniwise,it is developed OGO studio"
+fs.writeFile('file.txt',content,err => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+
+})
+
+fs.readFile('file.txt','utf8',(err,data)=> {
+    if (err){
+        console.error(err);
+    return;
+}
+
+    console.log(data);
+    
+})
