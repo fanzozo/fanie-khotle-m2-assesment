a70e30b5d9cddbe09ac188fb669c69fa21fae6ea
fanzozo <48625823+fanzozo@users.noreply.github.com>
2022-10-17 10:46:36 +0200

    initial commit
    mtn assesment 2a

---
+++ app.js
@@ -0,0 +1,8 @@
+window.alert('laoding')
+const Name = "Fanie Khotle"
+const school = "Mtn App Academy"
+const username = "fanzozo" 
+let intro = () => {
+    console.log(`Hi I'm ${Name}, a student from ${school} and my github username is ${username}`);
+}
+window.onload= intro();
